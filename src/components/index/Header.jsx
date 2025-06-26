27c342e8ba969f7c118c66474c960c0eb95572cf
Jonelle Cubio <girlthatloveswriting@gmail.com>
2025-06-26 15:53:34 +0800

    Remove dark mode

--- src/components/index/Header.jsx
+++ src/components/index/Header.jsx
@@ -2,19 +2,12 @@ import search from '../../assets/search.svg';
 import cart from '../../assets/cart.svg';
 import profile from '../../assets/profile.svg';
 import cartBlack from '../../assets/black.svg';
-import { use, useState } from 'react';
-import { FaMoon, FaSun } from 'react-icons/fa';
+
 
 
 
 export function Header() {
 
-  const [isDark, setIsDark] = useState(false);
-  const toggleDarkMode = () => {
-    setIsDark(!isDark);
-    document.body.className = isDark ? 'light-mode' : 'dark-mode';
-
-  };
 
   return(
     <main className="header-main">
@@ -53,13 +46,6 @@ export function Header() {
             <img src={profile} alt="Profile" className='header-profile' />
         </div>
 
-        {/* Dark Mode Toggle */}
-        <div className="header-darkmode-toggle">
-         <FaMoon color={isDark ? 'white' : 'black'} size={20} onClick={toggleDarkMode} className="header-darkmode-icon" />
-
-         <FaSun color={isDark ? 'white' : 'black'} size={20} onClick={toggleDarkMode} className="header-darkmode-icon" />
-          
-        </div>
         
       </div>
     </main>
