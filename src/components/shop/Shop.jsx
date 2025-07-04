5f910f906886c96ed966c7333cf54258b8b18f41
Jonelle Cubio <girlthatloveswriting@gmail.com>
2025-07-04 15:04:30 +0800

    Added shop breadcrumbs

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,22 +1,13 @@
 import { Header } from "../index/Header";
 import {Filters} from "../shop/Filters.jsx";
-import rightside from "../../assets/rightside.svg";
-import { Link } from "react-router-dom";
-import axios from "axios";
+import { ShopBreadcrumbs } from "./ShopBreadcrumbs.jsx";
 
 export function Shop() {
   return(
    
       <>
       <Header />
-      <div className="shop-con">
-        <Link to="/"><div className="shop-list">Home</div></Link>
-        <div className="shop-link-img-con">
-          <img src={rightside} alt="right arrow" className="shop-link-img" />
-        </div>
-        <Link to="/shop"><div className="shop-list">Shop</div></Link>
-      </div>
-
+      <ShopBreadcrumbs />
       <Filters />
       </>
     
---
+++ src/components/shop/ShopBreadcrumbs.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import rightside from "../../assets/rightside.svg";
+
+export function ShopBreadcrumbs() {
+  return(
+    <main className="shop-breadcrumbs-main">
+      <div className="shop-con">
+        <Link to="/"><div className="shop-list">Home</div></Link>
+        <div className="shop-link-img-con">
+          <img src={rightside} alt="right arrow" className="shop-link-img" />
+        </div>
+        <Link to="/shop"><div className="shop-list">Shop</div></Link>
+      </div>
+    </main>
+  );
+}
